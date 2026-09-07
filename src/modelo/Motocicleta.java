c2d4634294290c6ef697755a5438fa7afd585302
Maria Alveo <mariarosaalveo@gmail.com>
2026-09-06 23:26:05 -0500

    Se agrega la subclase Motocicleta y lectura de registros MOTOCICLETA

---
+++ src/modelo/Motocicleta.java
@@ -0,0 +1,14 @@
+package modelo;
+
+public class Motocicleta extends Vehiculo{
+private final double cilindrada;
+
+public Motocicleta (String marca, String modelo, int anio, double precio, double cilindrada){
+    super(marca, modelo, anio, precio);
+    this.cilindrada = cilindrada;
+}
+@Override
+    protected String getCaracteristica(){
+    return "Cilindrada: "+cilindrada+ "cc";
+}
+}
